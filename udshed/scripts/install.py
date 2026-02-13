c30c1196c6bab232c21c4d2a8f146e789c8cba7d
Cedric-Yaba-In <c.nguendap@yaba-in.com>
2026-02-13 06:57:13 +0100

    fix: end basis config

--- udshed/scripts/install.py
+++ udshed/scripts/install.py
@@ -3,7 +3,7 @@ import udshed.utils.time_utils as time_utils
 
 def create_default_data():
     default_acadamic_year = time_utils.get_default_academic_year()
-    create_default_period()
+    # create_default_period()
 
 def create_default_period():
     if not frappe.db.exists("Planning Period",{"heure_de_debut":"08:00","heure_de_fin":"12:00"}):
@@ -11,14 +11,16 @@ def create_default_period():
             "doctype":"Planning Period",
             "heure_de_debut":"08:00",
             "heure_de_fin":"12:00",
-            "libelle":"Matin"
+            "libelle":"Matin",
+            "position":0
         }).insert(ignore_permissions=True)
     if not frappe.db.exists("Planning Period",{"heure_de_debut":"13:30","heure_de_fin":"16:45"}):
         frappe.get_doc({
             "doctype":"Planning Period",
             "heure_de_debut":"13:30",
             "heure_de_fin":"16:45",
-            "libelle":"Soir"
+            "libelle":"Soir",
+            "position":1
         }).insert(ignore_permissions=True)
 
 def load_json(doctype, path):

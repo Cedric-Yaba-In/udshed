3bfff6df2e235da319d284e8a1880eabd7ec3077
Cedric-Yaba-In <c.nguendap@yaba-in.com>
2026-02-15 06:52:50 +0100

    fix:add calendar planning doctype in code

---
+++ udshed/udshed/doctype/calendar_planing/calendar_planing.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Cédric Nguendap Bedjama and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Calendar Planing", {
+// 	refresh(frm) {
+
+// 	},
+// });
